669aa7cc3482d6068fb6645266c7775e468711de
Djalilov Ali <MostWanted2014@ukr.net>
2021-06-14 00:27:37 +0300

    creat new calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function Calculator (number) {
+
+  this.result = number;
+  this.sum = (value) => (this.result += value);
+  this.mult = (value) => (this.result *= value);
+  this.sub = (value) => (this.result -= value);
+  this.div = (value) => (this.result /= value);
+
+}
+
+const calc = new Calculator(10);
+
+ console.log(calc.sum(5)); 
+console.log(calc.mult(10)); 
+console.log(calc.sub(40)); 
+console.log(calc.div(10)); 
+console.log(calc.result);
